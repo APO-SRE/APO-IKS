2fa3532aa95da6a9f2ebea5a3b990772b2b3a361
Ed Barberis <ed.barberis@gmail.com>
2022-03-02 10:29:38 -0500

    Updated the FSO Lab DevOps build and deployment documentation.

--- output.tf
+++ output.tf
@@ -1,3 +1,5 @@
+# Outputs ------------------------------------------------------------------------------------------
 output "k8s_cluster_moid" {
-  value = module.iks_cluster.k8s_cluster_moid
+  description = "Kubernetes IKS managed object ID."
+  value       = module.iks_cluster.k8s_cluster_moid
 }
